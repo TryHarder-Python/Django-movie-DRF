8608efd038ef87f10fb454ae0658c4f11da2a8cd
Gleb Maximenko <gleb.white90@gmail.com>
2022-01-03 19:50:12 +0200

    Add ReviewSerializer and ReviewCreateSerializer for post and get Reviews

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer
 
 
 class MovieListView(APIView):
@@ -16,7 +16,18 @@ class MovieListView(APIView):
 
 class MovieDetailView(APIView):
     """Вывод фильма"""
+
     def get(self, request, pk):
         movie = Movie.objects.get(id=pk, draft=False)
         serializer = MovieDetailSerializer(movie)
         return Response(serializer.data)
+
+
+class ReviewCreateView(APIView):
+    """Добавление отзыва к фильму"""
+
+    def post(self, request):
+        review = ReviewCreateSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+        return Response(status=201)
